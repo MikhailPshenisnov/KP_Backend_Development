6dca9054b6fb8a209f0064c00a5c8de08e4f5b20
MikhailPshenisnov <mishail.1703@gmail.com>
2024-12-23 22:01:39 +0300

    Очистка файлов микросервисов
    Из файлов микросервисов вырезаны вызовы неиспользуемых сервисов, в микросервисе авторизации исправлена строка получения к базе данных на корректную

--- Postomat/Postomat.AuthorizationMicroservice/Program.cs
+++ Postomat/Postomat.AuthorizationMicroservice/Program.cs
@@ -10,46 +10,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// Services for controllers
+// Services for consumers
 builder.Services.AddTransient<IAuthorizationService, AuthorizationService>();
-builder.Services.AddTransient<ICustomerService, CustomerService>();
-builder.Services.AddTransient<IDeliveryService, DeliveryService>();
-builder.Services.AddTransient<ILogsService, LogsService>();
-builder.Services.AddTransient<IOrderPlansService, OrderPlansService>();
-builder.Services.AddTransient<IOrdersService, OrdersService>();
-builder.Services.AddTransient<IPostomatsService, PostomatsService>();
-builder.Services.AddTransient<IRolesService, RolesService>();
 builder.Services.AddTransient<IUsersService, UsersService>();
 
 // Services for repositories
-builder.Services.AddTransient<ICellsRepository, CellsRepository>();
-builder.Services.AddTransient<ILogsRepository, LogsRepository>();
-builder.Services.AddTransient<IOrderPlansRepository, OrderPlansRepository>();
-builder.Services.AddTransient<IOrdersRepository, OrdersRepository>();
-builder.Services.AddTransient<IPostomatsRepository, PostomatsRepository>();
-builder.Services.AddTransient<IRolesRepository, RolesRepository>();
 builder.Services.AddTransient<IUsersRepository, UsersRepository>();
 
 // Database context
 builder.Services.AddDbContext<PostomatDbContext>();
-
-/* TODO */
-// CORS policy
-builder.Services.AddCors(options => options.AddPolicy
-    (
-        "AuthorizationMicroservicePolicy", b => b
-            .WithOrigins
-            (
-                "https://localhost:7191", // base application address
-                "https://localhost:7193" // logging microservice address
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials()
-    )
-);
 
 // Message broker
 builder.Services.AddMassTransit(x =>
@@ -70,13 +40,6 @@ builder.Services.AddMassTransit(x =>
 });
 
 var app = builder.Build();
-
-// Swagger
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
--- Postomat/Postomat.LoggingMicroservice/Program.cs
+++ Postomat/Postomat.LoggingMicroservice/Program.cs
@@ -10,45 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// Services for controllers
-builder.Services.AddTransient<ICustomerService, CustomerService>();
-builder.Services.AddTransient<IDeliveryService, DeliveryService>();
-builder.Services.AddTransient<IAuthorizationService, AuthorizationService>();
+// Services for consumers
 builder.Services.AddTransient<ILogsService, LogsService>();
-builder.Services.AddTransient<IOrderPlansService, OrderPlansService>();
-builder.Services.AddTransient<IOrdersService, OrdersService>();
-builder.Services.AddTransient<IPostomatsService, PostomatsService>();
-builder.Services.AddTransient<IRolesService, RolesService>();
-builder.Services.AddTransient<IUsersService, UsersService>();
 
 // Services for repositories
-builder.Services.AddTransient<ICellsRepository, CellsRepository>();
 builder.Services.AddTransient<ILogsRepository, LogsRepository>();
-builder.Services.AddTransient<IOrderPlansRepository, OrderPlansRepository>();
-builder.Services.AddTransient<IOrdersRepository, OrdersRepository>();
-builder.Services.AddTransient<IPostomatsRepository, PostomatsRepository>();
-builder.Services.AddTransient<IRolesRepository, RolesRepository>();
-builder.Services.AddTransient<IUsersRepository, UsersRepository>();
 
 // Database context
 builder.Services.AddDbContext<PostomatDbContext>();
-
-// CORS policy
-builder.Services.AddCors(options => options.AddPolicy
-    (
-        "AuthorizationMicroservicePolicy", b => b
-            .WithOrigins
-            (
-                "https://localhost:7191", // base application address
-                "https://localhost:7192" // authorization microservice address
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials()
-    )
-);
 
 // Message broker
 builder.Services.AddMassTransit(x =>
@@ -72,12 +42,6 @@ builder.Services.AddMassTransit(x =>
 });
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
